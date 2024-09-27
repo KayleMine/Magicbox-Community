f2e78b7c53e13c3be028ef8b4f613bf0c11a6b40
KayleMine <60858771+KayleMine@users.noreply.github.com>
2024-09-27 04:18:16 +0300

    Update virtual pseudo LOS

--- !MagicBoxCommunity/Butwhy/Core/system/environment/virtual.lua
+++ !MagicBoxCommunity/Butwhy/Core/system/environment/virtual.lua
@@ -51,19 +51,86 @@ function dark_addon.environment.virtual.resolvers.unit(unitA, unitB)
     return unitB, healthB
   end
 end
+	
 
+if not tLOS then tLOS = {} end
+local updateRate = 3
+local currentTarget;
+
+-- LineOfSight function definition
+function LineOfSight(target)
+currentTarget = target
+    if #tLOS > 0 then
+        if tLOS[1].unit == target then
+            -- Return true if target is in LoS
+            return true
+        end
+    end
+    return false
+end
+
+local function fLOSOnEvent(event, spellFailed, errorMessage, unit)
+            if spellFailed == SPELL_FAILED_LINE_OF_SIGHT or
+               spellFailed == SPELL_FAILED_NOT_INFRONT or
+               spellFailed == SPELL_FAILED_OUT_OF_RANGE or
+               spellFailed == SPELL_FAILED_UNIT_NOT_INFRONT or
+               spellFailed == SPELL_FAILED_UNIT_NOT_BEHIND or
+               spellFailed == SPELL_FAILED_NOT_BEHIND or
+               spellFailed == SPELL_FAILED_MOVING or
+               spellFailed == SPELL_FAILED_IMMUNE or
+               spellFailed == SPELL_FAILED_FLEEING or
+               spellFailed == SPELL_FAILED_BAD_TARGETS or
+               spellFailed == SPELL_FAILED_STUNNED or
+               spellFailed == SPELL_FAILED_SILENCED or
+               spellFailed == SPELL_FAILED_NOT_IN_CONTROL or
+               spellFailed == SPELL_FAILED_VISION_OBSCURED or
+               spellFailed == SPELL_FAILED_DAMAGE_IMMUNE or
+               spellFailed == SPELL_FAILED_CHARMED then
+ 
+			if currentTarget then
+				tLOS = {}
+				tinsert(tLOS, {unit = currentTarget, time = GetTime()})
+			end
+            end
+end
+
+local function cleanLOSTable()
+    if #tLOS > 0 then
+        local currentTime = GetTime()
+        for i = #tLOS, 1, -1 do
+            if (currentTime > tLOS[i].time + updateRate) then
+                table.remove(tLOS, i) 
+            end
+        end
+    end
+end
+
+C_Timer.NewTicker(3, function() -- uh eh umm
+    cleanLOSTable()
+end)
+
+dark_addon.Listener:Add("FacingCheck", "UI_ERROR_MESSAGE", function(...)
+    fLOSOnEvent(...)
+end)
+
+
+dark_addon.LineOfSight = LineOfSight;
+ 
 function dark_addon.environment.virtual.resolvers.party(members)
   local lowest = 'player'
   local lowest_health
   for i = 1, (members - 1) do
     local unit = 'party' .. i
-    if not UnitCanAttack('player', unit) and UnitInRange(unit) and not UnitIsDeadOrGhost(unit) and (not dark_addon.environment.virtual.exclude_tanks or not dark_addon.environment.virtual.resolvers.tank(unit)) then
-      if not lowest then
-        lowest, lowest_health = dark_addon.environment.virtual.resolvers.unit(unit, 'player')
-      else
-        lowest, lowest_health = dark_addon.environment.virtual.resolvers.unit(unit, lowest)
-      end
-    end
+ 
+		if not UnitCanAttack('player', unit) and UnitIsVisible(unit) and UnitIsConnected(unit) and UnitInRange(unit) and not UnitIsDeadOrGhost(unit) and not LineOfSight(unit)
+			and (not dark_addon.environment.virtual.exclude_tanks or not dark_addon.environment.virtual.resolvers.tank(unit)) then
+		  if not lowest then
+			lowest, lowest_health = dark_addon.environment.virtual.resolvers.unit(unit, 'player')
+		  else
+			lowest, lowest_health = dark_addon.environment.virtual.resolvers.unit(unit, lowest)
+		  end
+		end
+ 
   end
   return lowest
 end
@@ -73,15 +140,19 @@ function dark_addon.environment.virtual.resolvers.raid(members)
   local lowest_health
   for i = 1, members do
     local unit = 'raid' .. i
-    if not UnitCanAttack('player', unit) and UnitInRange(unit) and not UnitIsDeadOrGhost(unit) and (not dark_addon.environment.virtual.exclude_tanks or not dark_addon.environment.virtual.resolvers.tank(unit)) then
+
+	if not UnitCanAttack('player', unit) and UnitIsVisible(unit) and UnitIsConnected(unit) and UnitInRange(unit) and not UnitIsDeadOrGhost(unit) and not LineOfSight(unit)
+		and (not dark_addon.environment.virtual.exclude_tanks or not dark_addon.environment.virtual.resolvers.tank(unit)) then
       if not lowest then
         lowest, lowest_health = unit, UnitHealth(unit)
       else
         lowest, lowest_health = dark_addon.environment.virtual.resolvers.unit(unit, lowest)
       end
     end
-  end
-  return lowest
+
+
+ return lowest
+end
 end
 
 function dark_addon.environment.virtual.resolvers.tank(unit)
@@ -91,7 +162,7 @@ end
 function dark_addon.environment.virtual.resolvers.tanks(assignment)
   local members = GetNumGroupMembers()
   local group_type = GroupType()
-  if UnitExists('focus') and not UnitCanAttack('player', 'focus') and not UnitIsDeadOrGhost('focus') and assignment == 'MAINTANK' then
+  if UnitExists('focus') and UnitIsVisible(unit) and UnitIsConnected(unit) and not UnitCanAttack('player', 'focus') and not UnitIsDeadOrGhost('focus') and assignment == 'MAINTANK' then
     return 'focus'
   end
   if group_type ~= 'solo' then
