51bd0cc4b3147af0fd18d6730f95214728ceb008
KayleMine <60858771+KayleMine@users.noreply.github.com>
2024-09-30 22:57:53 +0300

    Update modifiers

--- !MagicBoxCommunity/Butwhy/Core/system/environment/modifiers.lua
+++ !MagicBoxCommunity/Butwhy/Core/system/environment/modifiers.lua
@@ -50,6 +50,119 @@ function modifiers:MouseButton5()
   return IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
 end
 
+-- MouseButton3 with modifiers
+function modifiers:SMB3()
+  return IsShiftKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:CtrlMB3()
+  return IsControlKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:AltMB3()
+  return IsAltKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RSMB3()
+  return IsRightShiftKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RCtrlMB3()
+  return IsRightControlKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RAltMB3()
+  return IsRightAltKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LSMB3()
+  return IsLeftShiftKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LCtrlMB3()
+  return IsLeftControlKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LAltMB3()
+  return IsLeftAltKeyDown() and IsMouseButtonDown(3) and GetCurrentKeyBoardFocus() == nil
+end
+
+-- MouseButton4 with modifiers
+function modifiers:SMB4()
+  return IsShiftKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:CtrlMB4()
+  return IsControlKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:AltMB4()
+  return IsAltKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RSMB4()
+  return IsRightShiftKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RCtrlMB4()
+  return IsRightControlKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RAltMB4()
+  return IsRightAltKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LSMB4()
+  return IsLeftShiftKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LCtrlMB4()
+  return IsLeftControlKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LAltMB4()
+  return IsLeftAltKeyDown() and IsMouseButtonDown(4) and GetCurrentKeyBoardFocus() == nil
+end
+
+-- MouseButton5 with modifiers
+function modifiers:SMB5()
+  return IsShiftKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:CtrlMB5()
+  return IsControlKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:AltMB5()
+  return IsAltKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RSMB5()
+  return IsRightShiftKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RCtrlMB5()
+  return IsRightControlKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:RAltMB5()
+  return IsRightAltKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LSMB5()
+  return IsLeftShiftKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LCtrlMB5()
+  return IsLeftControlKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+function modifiers:LAltMB5()
+  return IsLeftAltKeyDown() and IsMouseButtonDown(5) and GetCurrentKeyBoardFocus() == nil
+end
+
+
+
 dark_addon.environment.hooks.modifier = setmetatable({}, {
   __index = function(t, k)
     if modifiers[k] then
